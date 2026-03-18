e2c68c5c259cd9fa737b5ff5dbfd32fa657bf1be
IMTIAZ-X <godmanbd@gmail.com>
2026-03-18 22:38:08 +0600

    Create Project

---
+++ app/src/main/kotlin/com/example/compose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.Alignment
+import com.example.compose.ui.theme.ComposeApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeApp {
+                myapp()
+            }
+        }
+    }
+}
+
+@Composable
+fun myapp{
+Text(
+text = "Hello World"
+)
+}
