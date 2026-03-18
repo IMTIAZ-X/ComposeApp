7ed9c6a4c66d2831df6f486da9c4c034ad6f840d
IMTIAZ-X <godmanbd@gmail.com>
2026-03-18 22:58:08 +0600

    Create Project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.navigation3)
+  //  implementation(libs.androidx.navigation3)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
